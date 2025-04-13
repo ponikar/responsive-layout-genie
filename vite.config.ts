52f9b3c1ea7fe553038ace21eba6dc5f2c0f3be3
Darshan Ponikar <drsofficial3k@gmail.com>
2025-04-13 15:20:37 +0530

    feat: headers updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,38 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'unsafe-eval'; object-src 'none';",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Resource-Policy": "cross-origin",
+    },
   },
   plugins: [
     react(),
-    mode === 'development' &&
     componentTagger(),
-  ].filter(Boolean),
+    createHtmlPlugin({
+      inject: {
+        tags: [
+          {
+            tag: "meta",
+            attrs: {
+              "http-equiv": "Content-Security-Policy",
+              content:
+                "default-src 'self'; script-src 'self' 'unsafe-eval'; object-src 'none';",
+            },
+            injectTo: "head",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
